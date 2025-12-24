a78581fe7455ff5c92879387e3e32bbfacad23b1
HB2k <hb2k@bk.ru>
2025-12-24 20:52:18 +0400

    Refactor node mappings and update exports
    Removed specific node mappings for FlashVSR and updated __all__ export.
    
    Signed-off-by: HB2k <hb2k@bk.ru>

--- __init__.py
+++ __init__.py
@@ -1,17 +1,3 @@
-from .nodes import FlashVSRNode, FlashVSRNodeAdv, FlashVSRNodeInitPipe
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-# Node-ların sistemdəki (kod) adları - hamısının sonuna _Stable artırdım
-NODE_CLASS_MAPPINGS = {
-    "FlashVSR_Stable": FlashVSRNode,
-    "FlashVSR_Stable_Adv": FlashVSRNodeAdv,
-    "FlashVSR_Stable_Pipe": FlashVSRNodeInitPipe
-}
-
-# Node-ların menyuda görünən adları
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "FlashVSR_Stable": "FlashVSR Ultra-Fast (Stable)",
-    "FlashVSR_Stable_Adv": "FlashVSR Advanced (Stable)",
-    "FlashVSR_Stable_Pipe": "FlashVSR Init Pipe (Stable)"
-}
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
